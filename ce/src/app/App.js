e65d35e671a71aeb69a92f5d43109b70f8744318
michelangelo17 <michelangelo.markus@gmail.com>
2019-12-12 02:06:58 -0800

    first dropdown done

--- ce/src/app/App.js
+++ ce/src/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.scss'
+import FirstCurrency from './components/main/dropdowns/FirstCurrency'
 
 const App = () => {
   return (
     <div className='App'>
       <h1>Simple Currency Exchanger</h1>
+      <FirstCurrency />
     </div>
   )
 }
--- ce/src/app/App.test.js
+++ ce/src/app/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from '../redux/store'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
+test('renders title', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
   const title = getByText(/currency exchanger/i)
   expect(title).toBeInTheDocument()
 })
